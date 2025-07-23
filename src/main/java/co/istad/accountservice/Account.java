53df616a3ab8d3ebb8a832604db68b3b0294e96b
Sun Hour <sunhour.info@gmail.com>
2025-07-23 18:31:26 +0700

    first commit

---
+++ src/main/java/co/istad/accountservice/Account.java
@@ -0,0 +1,24 @@
+package co.istad.accountservice;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table (name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true,nullable = false)
+    private String actNo;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+}
